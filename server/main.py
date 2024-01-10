c13ddef13e3d6d5ed13b9e2509f27e5795954c2e
unknown <peteradekolu@gmail.com>
2024-01-10 02:58:56 +0100

    added changes

--- server/main.py
+++ server/main.py
@@ -50,7 +50,5 @@ app.include_router(acc_prov_router)
 app.include_router(explorer_router)
 app.include_router(admin_router)
 
-# app.mount("/static", StaticFiles(directory=BASEDIR+"\statics"), name="static")
-
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
